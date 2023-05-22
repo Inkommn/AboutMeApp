783d2b59a8d0ce1673d2f7d543b801e873a6e217
Shamkhan Mutuskhanov <spantset@icloud.com>
2023-05-22 11:18:30 +0300

    Initial Commit

---
+++ AboutMeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMeApp
+//
+//  Created by Shamkhan Mutuskhanov on 22.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
